f2a45b5bc094c9c9ca1d6a3180ec85db9c9c1edc
Naitik Shah <n@daaku.org>
2012-11-26 21:26:44 -0800

    more code

--- pie/pie_test.go
+++ pie/pie_test.go
@@ -1,9 +1,48 @@
 package pie_test
 
 import (
-  "github.com/daaku/pie/pie"
-  "testing"
+	"github.com/daaku/pie/pie"
+	"os"
+	"regexp"
+	"testing"
 )
 
+type TestCase struct {
+	Name string
+	Rule []pie.Rule
+}
+
+var cases = []TestCase{
+	TestCase{
+		Name: "base",
+		Rule: []pie.Rule{
+			&pie.ReplaceAll{
+				Target: regexp.MustCompile("hello"),
+				Repl:   []byte("goodbye"),
+			},
+		},
+	},
+}
+
+func (t TestCase) MakeTempCopy() string {
+	return "/home/naitik/usr/go/src/pkg/github.com/daaku/pie/pie/_tests/base/before"
+}
+
+func GetDataDir() string {
+	return "/home/naitik/usr/go/src/pkg/github.com/daaku/pie/pie/_tests"
+}
+
 func TestAll(t *testing.T) {
+	for _, test := range cases {
+		tmp := test.MakeTempCopy()
+		defer os.RemoveAll(tmp)
+		run := &pie.Run{
+			Root: tmp,
+			Rule: test.Rule,
+		}
+		err := run.Run()
+		if err != nil {
+			t.Fatal("run for %s failed: %s", test.Name, err)
+		}
+	}
 }
