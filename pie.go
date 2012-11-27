7b9270a894ad7e5b92075833098bad875e69abdd
Naitik Shah <n@daaku.org>
2012-11-26 22:14:36 -0800

    a minimal main

--- pie.go
+++ pie.go
@@ -1,10 +1,29 @@
 package main
 
 import (
-  "fmt"
-  "github.com/daaku/pie/pie"
+	"fmt"
+	"github.com/daaku/pie/pie"
+	"os"
+	"regexp"
 )
 
 func main() {
-  fmt.Println("pie")
+	if len(os.Args) != 4 {
+		fmt.Printf("usage: %s <directory> <target-regexp> <replace-pattern>\n", os.Args[0])
+		os.Exit(1)
+	}
+	r := &pie.Run{
+		Root: os.Args[1],
+		Rule: []pie.Rule{
+			&pie.ReplaceAll{
+				Target: regexp.MustCompile(os.Args[2]),
+				Repl:   []byte(os.Args[3]),
+			},
+		},
+	}
+	err := r.Run()
+	if err != nil {
+		fmt.Printf("error: %s", err)
+		os.Exit(1)
+	}
 }
