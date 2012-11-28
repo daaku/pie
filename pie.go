11f921bf4db0f9d4f0daab95a0a9ef2e5cd934fa
Naitik Shah <n@daaku.org>
2012-11-27 19:26:50 -0800

    accept rules via stdin as well

--- pie.go
+++ pie.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"github.com/daaku/pie/pie"
@@ -16,6 +17,35 @@ var (
 	filterRegexp = flag.String("filter", "", "file full path filter regexp")
 )
 
+func addFromStdin(r *pie.Run) {
+	reader := csv.NewReader(os.Stdin)
+	reader.Comma = '\t'
+	reader.Comment = '#'
+	reader.FieldsPerRecord = 2
+	reader.LazyQuotes = true
+	reader.TrimLeadingSpace = true
+	rules, err := reader.ReadAll()
+	if err != nil {
+		panic(fmt.Sprintf("failed reading rules from stdin: %s", err))
+	}
+	for _, rule := range rules {
+		r.Rule = append(r.Rule, &pie.ReplaceAll{
+			Target: regexp.MustCompile(rule[0]),
+			Repl:   []byte(rule[1]),
+		})
+	}
+}
+
+func addFromArgs(r *pie.Run, args []string) {
+	argl := len(args)
+	for x := 1; x < argl; x = x + 2 {
+		r.Rule = append(r.Rule, &pie.ReplaceAll{
+			Target: regexp.MustCompile(args[x]),
+			Repl:   []byte(args[x+1]),
+		})
+	}
+}
+
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(
@@ -28,10 +58,14 @@ func main() {
 	runtime.GOMAXPROCS(*goMaxProcs)
 	args := flag.Args()
 	argl := len(args)
-	if argl < 3 || argl%2 == 0 {
+	if argl%2 == 0 {
+		if argl != 1 {
+			fmt.Fprintf(os.Stderr, "target/replace must be in pairs\n")
+		}
 		flag.Usage()
 		os.Exit(1)
 	}
+
 	r := &pie.Run{
 		Root:     args[0],
 		Parallel: *parallelSize,
@@ -42,15 +76,19 @@ func main() {
 	if *filterRegexp != "" {
 		r.FileFilter = regexp.MustCompile(*filterRegexp)
 	}
-	for x := 1; x < argl; x = x + 2 {
-		r.Rule = append(r.Rule, &pie.ReplaceAll{
-			Target: regexp.MustCompile(args[x]),
-			Repl:   []byte(args[x+1]),
-		})
+	if argl < 3 {
+		addFromStdin(r)
+	} else {
+		addFromArgs(r, args)
+	}
+	if len(r.Rule) == 0 {
+		fmt.Fprintf(os.Stderr, "error: no rules provided on the command line or via stdin\n")
+		flag.Usage()
+		os.Exit(1)
 	}
 	err := r.Run()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s", err)
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(1)
 	}
 }
